b90a26d56ef0d297b2cef063298356e4c34af5aa
Lucas Lucena <ml.lucaslucena073@outlook.com>
2020-04-11 11:32:27 -0300

    rn projec created with new log box

---
+++ LIBRARY.AppNavBar/statusbar/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends Component{
+  render(){
+
+    return(
+
+      <>
+      </>
+
+    )
+
+  }
+} 
