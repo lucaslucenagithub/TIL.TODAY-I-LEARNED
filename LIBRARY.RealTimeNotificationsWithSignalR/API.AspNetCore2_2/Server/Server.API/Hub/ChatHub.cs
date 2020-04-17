028c159d77cecdb8e9ac9b6fc9fee3c5918d7c89
Lucas Lucena <ml.lucaslucena073@outlook.com>
2020-04-17 17:49:54 -0300

    added real time notifications project.

---
+++ LIBRARY.RealTimeNotificationsWithSignalR/API.AspNetCore2_2/Server/Server.API/Hub/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.API
+{
+    public class ChatHub : Hub
+    {
+        public void SendToChannel(string name, string message)
+        {
+            Clients.All.SendAsync("SendToChannel", name, message);
+        }
+    }
+}
