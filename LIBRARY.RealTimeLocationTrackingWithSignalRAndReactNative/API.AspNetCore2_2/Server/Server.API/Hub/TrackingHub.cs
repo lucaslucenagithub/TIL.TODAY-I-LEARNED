1c3c792c893975a0979723ce96ddcf0641cdf8d4
Lucas Lucena <ml.lucaslucena073@outlook.com>
2020-04-18 13:08:03 -0300

    receiving location from the client

---
+++ LIBRARY.RealTimeLocationTrackingWithSignalRAndReactNative/API.AspNetCore2_2/Server/Server.API/Hub/TrackingHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.API
+{
+    public class TrackingHub : Hub
+    {
+        public void SendToChannel(string latitude, string longitude)
+        {
+            Console.WriteLine($"{latitude}, {longitude}");
+            Clients.All.SendAsync("SendToChannel", latitude, longitude);
+        }
+    }
+}
